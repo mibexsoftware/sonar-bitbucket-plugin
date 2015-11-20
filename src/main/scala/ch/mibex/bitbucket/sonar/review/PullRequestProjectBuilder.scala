0e5df37b8fe46defb5aada9f3f431dfe9c676004
Michael Rüegg <rueegg.michael@gmail.com>
2015-11-20 14:32:04 +0100

    Check that SonarQube's preview, incremental or issues mode is used

--- src/main/scala/ch/mibex/bitbucket/sonar/review/PullRequestProjectBuilder.scala
+++ src/main/scala/ch/mibex/bitbucket/sonar/review/PullRequestProjectBuilder.scala
@@ -1,12 +1,35 @@
 package ch.mibex.bitbucket.sonar.review
 
+import ch.mibex.bitbucket.sonar.SonarBBPluginConfig
+import org.sonar.api.CoreProperties
 import org.sonar.api.batch.bootstrap.ProjectBuilder
+import org.sonar.api.config.Settings
 
-
-class PullRequestProjectBuilder(gitBaseDirResolver: GitBaseDirResolver) extends ProjectBuilder {
+// this builder is executed at the beginning of the SonarQube analysis
+class PullRequestProjectBuilder(pluginConfiguration: SonarBBPluginConfig,
+                                gitBaseDirResolver: GitBaseDirResolver,
+                                settings: Settings) extends ProjectBuilder {
 
   override def build(context: ProjectBuilder.Context): Unit = {
-    gitBaseDirResolver.init(context.projectReactor().getRoot.getBaseDir)
+    if (pluginConfiguration.isEnabled) {
+      checkAnalysisMode()
+      gitBaseDirResolver.init(context.projectReactor().getRoot.getBaseDir)
+    }
   }
+
+  private def checkAnalysisMode() {
+    require(isDryRunMode || isPreviewMode || isIncrementalMode || isIssuesMode,
+      """The plug-in only works in preview or issues mode. Please set "-Dsonar.analysis.mode" accordingly.""")
+  }
+
+  private def isIssuesMode = settings.getString(CoreProperties.ANALYSIS_MODE) == "issues"
+
+  private def isIncrementalMode =
+    settings.getString(CoreProperties.ANALYSIS_MODE) == CoreProperties.ANALYSIS_MODE_INCREMENTAL
+
+  private def isPreviewMode =
+    settings.getString(CoreProperties.ANALYSIS_MODE) == CoreProperties.ANALYSIS_MODE_PREVIEW
+
+  private def isDryRunMode = settings.getBoolean(CoreProperties.DRY_RUN)
 
 }
