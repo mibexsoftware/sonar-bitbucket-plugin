08378360b38876518c1c24e412db97dc510010f8
Michael Rüegg <rueegg.michael@gmail.com>
2016-06-20 11:07:42 +0200

    Minor code reformatting

--- src/main/scala/ch/mibex/bitbucket/sonar/review/PullRequestProjectBuilder.scala
+++ src/main/scala/ch/mibex/bitbucket/sonar/review/PullRequestProjectBuilder.scala
@@ -6,11 +6,9 @@ import org.sonar.api.batch.bootstrap.ProjectBuilder
 import org.sonar.api.config.Settings
 
 // this builder is executed at the beginning of the SonarQube analysis
-class PullRequestProjectBuilder(
-  pluginConfiguration: SonarBBPluginConfig,
-  gitBaseDirResolver: GitBaseDirResolver,
-  settings: Settings
-) extends ProjectBuilder {
+class PullRequestProjectBuilder(pluginConfiguration: SonarBBPluginConfig,
+                                gitBaseDirResolver: GitBaseDirResolver,
+                                settings: Settings) extends ProjectBuilder {
 
   override def build(context: ProjectBuilder.Context): Unit = {
     if (pluginConfiguration.isEnabled) {
