47bc4785aeadfded7bde79175046753a4354f4b9
Divyakumar <divyakumarjain@gmail.com>
2018-02-06 13:56:30 +0530

    Hybris Git Directory Support

--- src/main/scala/ch/mibex/bitbucket/sonar/SonarBBPluginConfig.scala
+++ src/main/scala/ch/mibex/bitbucket/sonar/SonarBBPluginConfig.scala
@@ -32,6 +32,8 @@ class SonarBBPluginConfig(settings: Settings, server: Server) {
 
   def sonarApprovalSeverityLevel(): String = settings.getString(SonarBBPlugin.SonarUnapprovalSeverityLevel)
 
+  def gitBaseDir(): String = settings.getString(SonarBBPlugin.BitbucketGitBaseDir)
+
   def branchName(): String = {
     var branchName = settings.getString(SonarBBPlugin.BitbucketBranchName)
     Option(branchName) foreach { _ =>
